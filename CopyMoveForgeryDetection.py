ee7abc33a82e527bf1447c5339c749dce893e93d
AshrayGattani <ashrayg5@gmail.com>
2023-09-04 11:02:04 +0530

    acc error

--- CopyMoveForgeryDetection.py
+++ CopyMoveForgeryDetection.py
@@ -62,8 +62,8 @@ def OpenShowImage():
 	img=cv2.imread('{}'.format(filename),0)
 
 def AccuracyTest():
-	filename = filedialog.askopenfilename(initialdir = "../Forged%20Images/",title ="Open File",filetypes = (("png files","*.png"),("bmp files","*.bmp"),("jpeg files","*.jpg"),("All Files","*.*")))
-	img_for_accuracy = cv2.imread(filename,0)
+	filename1 = filedialog.askopenfilename(initialdir = "../Forged%20Images/",title ="Open File",filetypes = (("png files","*.png"),("bmp files","*.bmp"),("jpeg files","*.jpg"),("All Files","*.*")))
+	img_for_accuracy = cv2.imread(filename1,0)
 	dp=0
 	yp=0
 	yn=0
